4c9360303ad9adb643821fcb49449f5c52e92a5d
B.Ritesh <bonthalamadhavi1@gmail.com>
2025-06-01 22:36:38 +0530

    Update CV download functionality

--- src/App.tsx
+++ src/App.tsx
@@ -10,29 +10,19 @@ import Projects from './components/Projects';
 import Featured from './components/Featured';
 import Blog from './components/Blog';
 import Contact from './components/Contact';
-import BackgroundToggle from './components/BackgroundToggle';
-import DownloadCV from './components/DownloadCV';
 import Footer from './components/Footer';
 import ThreeBackground from './components/ThreeBackground';
-import DynamicGradient from './components/DynamicGradient';
 
 function App() {
-  const [viewMode, setViewMode] = useState<'default' | 'ai'>('default');
-
   useEffect(() => {
     document.body.classList.add('dark');
   }, []);
-
-  const handleBackgroundToggle = (type: 'default' | 'ai') => {
-    setViewMode(type);
-  };
 
   return (
     <div className="relative min-h-screen overflow-hidden font-poppins text-white">
       {/* Background Component */}
       <div className="absolute inset-0 z-0">
-        {viewMode === 'default' && <ThreeBackground />}
-        {viewMode === 'ai' && <DynamicGradient />}
+        <ThreeBackground />
       </div>
 
       {/* Main Content */}
@@ -47,8 +37,6 @@ function App() {
         <Featured />
         <Blog />
         <Contact />
-        <BackgroundToggle onToggle={handleBackgroundToggle} currentType={viewMode} />
-        <DownloadCV />
         <Footer />
       </div>
     </div>
